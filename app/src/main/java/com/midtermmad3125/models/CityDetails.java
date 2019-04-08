aeb64f9d9a5304d5056ab11f0920f4e09a4690f2
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-04-08 19:16:54 -0400

    MainCity Activity design and navigation set it up.

---
+++ app/src/main/java/com/midtermmad3125/models/CityDetails.java
@@ -0,0 +1,4 @@
+package com.midtermmad3125.models;
+
+public class CityDetails {
+}
--- app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
+++ app/src/main/java/com/midtermmad3125/ui/MainCityActivity.java
@@ -1,19 +1,44 @@
 package com.midtermmad3125.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.midtermmad3125.R;
 import com.midtermmad3125.utils.ReadJSONUtils;
 
 public class MainCityActivity extends AppCompatActivity
 {
+    Button buttonAdd;
+    TextView textCityName;
+    TextView textLat;
+    TextView textLng;
+    TextView textCountryCode;
+    TextView textPopulation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textCityName = findViewById(R.id.textCityName);
+        textLat = findViewById(R.id.textLatitude);
+        textLng = findViewById(R.id.textLongitude);
+        textCountryCode = findViewById(R.id.textCountryCode);
+        textPopulation = findViewById(R.id.textPopulation);
+        buttonAdd = findViewById(R.id.buttonAdd);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainCityActivity.this, WeatherListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
